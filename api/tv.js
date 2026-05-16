fa4093da97995a836a2e7edb29753ac6dd5ec4c6
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 20:57:03 +0530

    Create tv.js

---
+++ api/tv.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const type = req.query.type || "popular";
+  const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
+
+  const url = `https://api.themoviedb.org/3/tv/${type}?api_key=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
+}
