3cd8e1987967548f0094d831ae925ad03955fb70
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 20:57:34 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -1,7 +1,5 @@
 export default async function handler(req, res) {
-
   const type = req.query.type || "popular";
-
   const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
 
   const url = `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}`;
@@ -12,9 +10,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
-
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch TMDB data" });
+  } catch (e) {
+    res.status(500).json({ error: "API failed" });
   }
-
 }
