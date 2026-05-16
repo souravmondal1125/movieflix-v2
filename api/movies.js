012998cb2d4fa03b153bbcc8c5c636a9139f672f
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 21:10:38 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -4,13 +4,9 @@ export default async function handler(req, res) {
 
   const url = `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+  const response = await fetch(url);
+  const data = await response.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ error: "API failed" });
-  }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(200).json(data);
 }
