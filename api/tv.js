b961efbee4c70f83afbb384acdba8119c7b736d9
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 21:56:23 +0530

    Update tv.js

--- api/tv.js
+++ api/tv.js
@@ -1,11 +1,11 @@
 export default async function handler(req, res) {
-  const type = req.query.type || "popular";
+  const page = req.query.page || 1;
   const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
 
-  const url = `https://api.themoviedb.org/3/tv/${type}?api_key=${apiKey}`;
+  const url = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&page=${page}`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+  const r = await fetch(url);
+  const data = await r.json();
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json(data);
