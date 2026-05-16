62a562c9009a043ff5c6a1511c90fd3909c8372b
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 21:50:45 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-  const type = req.query.type || "popular";
+  const page = req.query.page || 1;
   const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
 
-  const url = `https://api.themoviedb.org/3/movie/${type}?api_key=${apiKey}`;
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}&page=${page}`;
 
   const response = await fetch(url);
   const data = await response.json();
