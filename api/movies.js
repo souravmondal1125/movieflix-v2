4eb7159aa2e874b0c285a90b2db39b247150a3b9
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 21:55:28 +0530

    Update movies.js

--- api/movies.js
+++ api/movies.js
@@ -2,10 +2,10 @@ export default async function handler(req, res) {
   const page = req.query.page || 1;
   const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
 
-  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}&page=${page}`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`;
 
-  const response = await fetch(url);
-  const data = await response.json();
+  const r = await fetch(url);
+  const data = await r.json();
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json(data);
