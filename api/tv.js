11bd269af5bfd0be5de1236f4a618c95b54863f3
Sourav Mondal <163639467+souravmondal1125@users.noreply.github.com>
2026-05-16 21:33:29 +0530

    Update tv.js

--- api/tv.js
+++ api/tv.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const type = req.query.type || "popular";
   const apiKey = "4c63ed0d29a4da8c6f3386e07b8b4c56";
 
-  const url = `https://api.themoviedb.org/3/tv/${type}?apiKey=${apiKey}`;
+  const url = `https://api.themoviedb.org/3/tv/${type}?api_key=${apiKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
